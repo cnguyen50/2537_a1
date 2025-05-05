e6ed935dfb71e845ceeaa732f69228eb037ca86e
cnguyen50 <calvinnguyen50@gmail.com>
2025-05-04 23:56:03 -0700

    Third time debugging

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,28 +5,15 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
 
-console.log('Connecting to MongoDB host:', mongodb_host);
+console.log('🔍 Connecting to MongoDB host:', mongodb_host);
 
 const MongoClient = require("mongodb").MongoClient;
-
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}`
                 + `@${mongodb_host}/${mongodb_database}`
-                + `?retryWrites=true&w=majority`;
-
-var database = new MongoClient(atlasURI, {});
-
-// Added the tlsInsecure flag here to bypass the SSL alert
-var client = new MongoClient(atlasURI, {
-    useNewUrlParser:     true,
-    useUnifiedTopology:  true,
-    tlsInsecure:         true
+                + `?retryWrites=true&w=majority&ssl=true`;
+var database = new MongoClient(atlasURI, {
+    tlsAllowInvalidCertificates: true,
+    tlsAllowInvalidHostnames:    true
 });
 
-client.connect()
-    .then(() => console.log(`✅ MongoDB connected to "${mongodb_database}"`))
-    .catch(err => {
-        console.error(' MongoDB connection error:', err);
-        process.exit(1);
-    });
-
-module.exports = { database };
+module.exports = {database};

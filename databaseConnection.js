a063d915d1ff4fc45903b66296596c3d89a60c15
cnguyen50 <calvinnguyen50@gmail.com>
2025-05-04 23:25:19 -0700

    added database name into uri

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+
+console.log('Connecting to MongoDB host:', mongodb_host);
 
 const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
+
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}`
+                + `@${mongodb_host}/${mongodb_database}`
+                + `?retryWrites=true&w=majority`;
+
 var database = new MongoClient(atlasURI, {});
 
 module.exports = {database};
